19a123191fbd3369223ab34a506e77e8ffd405cf
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2022-09-25 01:36:14 +0200

    updated

--- src/main/java/example/ExtraLogicMod.java
+++ src/main/java/example/ExtraLogicMod.java
@@ -1,8 +1,13 @@
 package example;
 
+import arc.Core;
+import arc.Events;
 import arc.util.Log;
+import arc.util.Time;
+import mindustry.game.EventType.ClientLoadEvent;
 import mindustry.logic.LExecutor;
 import mindustry.mod.Mod;
+import mindustry.ui.dialogs.BaseDialog;
 
 public class ExtraLogicMod extends Mod {
 
@@ -10,18 +15,19 @@ public class ExtraLogicMod extends Mod {
 		Log.info("Loaded ExampleJavaMod constructor.");
 		ReflectionHelper.set(LExecutor.class, "maxInstructions", null, 100000);
 //		// listen for game load event
-//		Events.on(ClientLoadEvent.class, e -> {
-//			// show dialog upon startup
-//			Time.runTask(10f, () -> {
-//				BaseDialog dialog = new BaseDialog("frog");
-//				dialog.cont.add("behold").row();
-//				// mod sprites are prefixed with the mod name (this mod is called
-//				// 'example-java-mod' in its config)
-//				dialog.cont.image(Core.atlas.find("mindustry-extra-logic-frog")).pad(20f).row();
-//				dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-//				dialog.show();
-//			});
-//		});
+		Events.on(ClientLoadEvent.class, e -> {
+			// show dialog upon startup
+			Time.runTask(10f, () -> {
+				BaseDialog dialog = new BaseDialog("frog");
+				dialog.cont.add("behold").row();
+				// mod sprites are prefixed with the mod name (this mod is called
+				// 'example-java-mod' in its config)
+				dialog.cont.image(Core.atlas.find("mindustry-extra-logic-frog")).pad(20f).row();
+				dialog.cont.button("New Processor Instruction Limit: " + LExecutor.maxInstructions, dialog::hide)
+						.size(100f, 50f);
+				dialog.show();
+			});
+		});
 
 	}
 
