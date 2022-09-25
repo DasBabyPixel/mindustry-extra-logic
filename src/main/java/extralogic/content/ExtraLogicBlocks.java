9cd8600e5dd511d7b70091f287cf090462b0a866
dasba <dasba@LComputer.fritz.box>
2022-09-26 01:35:18 +0200

    processors

--- src/main/java/extralogic/content/ExtraLogicBlocks.java
+++ src/main/java/extralogic/content/ExtraLogicBlocks.java
@@ -1,23 +1,53 @@
 package extralogic.content;
 
+import java.util.concurrent.atomic.AtomicReference;
+
+import arc.Events;
+import arc.func.Cons;
 import extralogic.logic.ExtraLogicBlock;
-import mindustry.content.Items;
-import mindustry.type.Category;
-import mindustry.type.ItemStack;
+import mindustry.content.Blocks;
+import mindustry.game.EventType;
 import mindustry.world.Block;
 
-public class ExtraLogicBlocks {
+public class ExtraLogicBlocks extends ExtraLogicContent {
 
-	public static Block micro_processor, extra_processor, hyper_processor;
+	public static Block micro_processor, logic_processor, hyper_processor;
 
 	public static void load() {
 		micro_processor = new ExtraLogicBlock("micro-processor") {
 
 			{
-				requirements(Category.logic, ItemStack.with(Items.lead, 50), true);
+				registerParent(this, Blocks.microProcessor);
+
 			}
 
 		};
+		logic_processor = new ExtraLogicBlock("logic-processor") {
+
+			{
+				registerParent(this, Blocks.logicProcessor);
+			}
+
+		};
+		hyper_processor = new ExtraLogicBlock("hyper-processor") {
+
+			{
+				registerParent(this, Blocks.hyperProcessor);
+			}
+
+		};
+	}
+
+	private static void registerParent(Block self, Block parent) {
+		self.requirements(parent.category, parent.requirements, unlockAll || parent.unlocked());
+		final AtomicReference<Cons<EventType.UnlockEvent>> ref = new AtomicReference<>(null);
+		final Cons<EventType.UnlockEvent> eventHandler = e -> {
+			if (e.content == parent) {
+				self.unlock();
+				Events.remove(EventType.UnlockEvent.class, ref.get());
+			}
+		};
+		ref.set(eventHandler);
 	}
 
 }
---
+++ src/main/java/extralogic/content/ExtraLogicContent.java
@@ -0,0 +1,7 @@
+package extralogic.content;
+
+public class ExtraLogicContent {
+
+	public static boolean unlockAll = true;
+
+}
