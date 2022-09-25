8cab51fd7762fefe69dd0b826298a1e461987a15
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2022-09-25 15:52:08 +0200

    updated

--- src/main/java/extralogic/logic/ExtraLogicProcessor.java
+++ src/main/java/extralogic/logic/ExtraLogicProcessor.java
@@ -1,11 +1,23 @@
 package extralogic.logic;
 
+import static mindustry.type.ItemStack.*;
+
+import mindustry.content.Items;
+import mindustry.type.Category;
 import mindustry.world.blocks.logic.LogicBlock;
 
 public class ExtraLogicProcessor extends LogicBlock {
 
 	public ExtraLogicProcessor() {
 		super("extra-logic-processor");
+		
+        requirements(Category.logic, with(Items.lead, 320, Items.silicon, 80, Items.graphite, 60, Items.thorium, 50));
+
+        instructionsPerTick = 35;
+
+        range = 8 * 28;
+
+        size = 2;
 	}
 
 }
